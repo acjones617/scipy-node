5666d892b63bd9c59edc161dc9d31a3ed776f4d2
Andrew Jones <acjones617@gmail.com>
2014-09-19 10:29:22 -0700

    Add api for non negative least squares

--- optimize/index.js
+++ optimize/index.js
@@ -3,5 +3,8 @@ var eng = require('./node/engine');
 module.exports = {
   minimize: function(func, options, callback) {
     eng.runPython('minimize', func, options, callback);
+  },
+  nonNegLeastSquares: function(A, b, callback) {
+    eng.runPython('nnls', A, b, callback);
   }
 }
