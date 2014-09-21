0614b4f335db83ea2fa4cc095e33853d052150dc
Andrew Jones <acjones617@gmail.com>
2014-09-20 20:24:34 -0700

    Add global minimize to exec py flowthrough, wrap json encoding in try block

--- optimize/py/exec.py
+++ optimize/py/exec.py
@@ -4,7 +4,6 @@ from sympy.utilities.lambdify import lambdify
 import json
 import argparse
 import ast
-
 parser = argparse.ArgumentParser()
 parser.add_argument('operation')
 parser.add_argument('a')
@@ -12,13 +11,19 @@ parser.add_argument('b')
 args   = parser.parse_args()
 operation = args.operation
 
-if operation == 'minimize':
+if operation == 'local' or operation == 'global':
     func      = args.a
     options   = json.JSONDecoder().decode(args.b)
-    results   = m.minimize_scalar(lambdify('x', func), options)
+    if operation == 'local':
+        results = m.local_minimize(lambdify('x', func), options)
+    if operation == 'global':
+        results = m.global_minimize(lambdify('x', func), options)
 elif operation == 'nnls':
     A = json.JSONDecoder().decode(args.a)
     b = json.JSONDecoder().decode(args.b)
     results = m.nnls(A, b)
-    
-print json.JSONEncoder().encode(results)
+
+try:
+    print json.JSONEncoder().encode(results)
+except Exception as e:
+    print e
