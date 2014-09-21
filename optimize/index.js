94352ff890da1dc1a83651b316eba519d9101243
Andrew Jones <acjones617@gmail.com>
2014-09-20 20:20:54 -0700

    Add localMin and globalMin to API

--- optimize/index.js
+++ optimize/index.js
@@ -1,9 +1,12 @@
 var eng = require('./node/engine');
 
 module.exports = {
-  minimize: function(func, options, callback) {
-    eng.runPython('minimize', func, options, callback);
+  localMinimize: function(func, options, callback) {
+    eng.runPython('local', func, options, callback);
   },
+  globalMinimize: function(func, options, callback) {
+    eng.runPython('global', func, options, callback);
+  },  
   nonNegLeastSquares: function(A, b, callback) {
     eng.runPython('nnls', A, b, callback);
   }
