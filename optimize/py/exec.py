16e10320080d22ac095ce6eccb5c82ca7e1365f0
Andrew Jones <acjones617@gmail.com>
2014-09-22 23:38:37 -0700

    Hook up with main curve_fit

--- optimize/py/exec.py
+++ optimize/py/exec.py
@@ -11,13 +11,21 @@ parser.add_argument('b')
 args   = parser.parse_args()
 operation = args.operation
 
-if operation == 'local' or operation == 'global':
+if operation in ['local', 'global', 'fit']:
     func      = args.a
     options   = json.JSONDecoder().decode(args.b)
     if operation == 'local':
         results = m.local_minimize(lambdify('x', func), options)
-    if operation == 'global':
+    elif operation == 'global':
         results = m.global_minimize(lambdify('x', func), options)
+    elif operation == 'fit':
+        num_args = options['numArgs']
+        args = ['x']
+        ch = 'a'
+        for i in range(0, num_args - 1):
+            args.append(ch)
+            ch = chr(ord(ch) + 1)
+        results = m.curve_fit(lambdify(tuple(args), func), options)
 elif operation == 'nnls':
     A = json.JSONDecoder().decode(args.a)
     b = json.JSONDecoder().decode(args.b)
