e1fc68556b8cdeacd521ee90d8675f1dbd192ec8
Andrew Jones <acjones617@gmail.com>
2014-09-24 14:31:17 -0700

    Add mocha tests for euclidean norm function

---
+++ optimize/test/euclideanNorm.js
@@ -0,0 +1,29 @@
+var opt = require('../index');
+var should = require('should');
+
+describe('euclidean norm', function(){
+  var result;
+  var A =  [[1 , 2 , -3],
+            [-4, 5 ,  6],
+            [7 , -8, -9]];
+  var b = [5, 3, -1];
+
+  before(function (done){
+    opt.minimizeEuclideanNorm(A, b, function(results) {
+      result = results;
+      done();
+    });
+
+    results = {
+      solution: [4.75, 2.5, 1.583333], 
+      residual: 0.0
+    }
+  });
+
+  it('correctly finds non-negative x to minimize the Eucliean Norm of the function', function() {
+    Math.round(result.solution[0] * 100).should.equal(475);
+    Math.round(result.solution[1] * 100).should.equal(250);
+    Math.round(result.solution[2] * 100).should.equal(158);
+    Math.round(result.residual * 100).should.equal(0);
+  });
+});
