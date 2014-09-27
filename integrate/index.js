3e2b118ff63144fa39f603c5f3285bb114800cb3
Andrew Jones <acjones617@gmail.com>
2014-09-27 11:36:03 -0700

    Swap integrate as the generalized version taking any number of variables

--- integrate/index.js
+++ integrate/index.js
@@ -1,7 +1,9 @@
 var eng = require('./node/engine');
 
-var integrate = module.exports = function (func, lower, upper, options, callback){
-  eng.runPython('single', func, options, callback, lower, upper);
+var integrate = module.exports = function (func, range, options, callback){
+  eng.runPython('multi', func, options, callback, range);
 };
 
-
+integrate.univariate = function (func, lower, upper, options, callback){
+  eng.runPython('single', func, options, callback, lower, upper);
+}
